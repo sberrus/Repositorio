5701bc0d38f7903fbab10e83cd822c065a12f9de
SamDev <samuelberrus@gmail.com>
2021-03-17 20:53:52 +0100

    Actualizacion total de la web

---
+++ src/js/scrollReveal.js
@@ -0,0 +1,17 @@
+/* Scripts Libreria Scroll Reveal */
+/* Para saber más revisar documentación oficial de la libreria => https://scrollrevealjs.org/guide/installation.html */
+
+//Método para disparar efecto a toda la página.
+ScrollReveal({
+    duration: 1500
+})
+
+//Aplicar reglas a ciertos elementos.
+ScrollReveal().reveal(".container",{
+    reset:true,
+    delay:150
+});
+ScrollReveal().reveal(".tech-items",{
+    reset:true,
+    interval: 150
+});
